22b6801a56c764f3af904c351bb6f898199516d5
therajusah <workwithrajukumar@gmail.com>
2024-05-25 23:48:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">
+        This is an Ecommerce Project using React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
