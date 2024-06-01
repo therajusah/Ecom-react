60df9cb626a8989645de2447486535b3d82c089a
therajusah <workwithrajukumar@gmail.com>
2024-06-01 23:18:35 +0530

    Removed 'h-full' class from outer div and duplicated inner divs, replaced h1 text with 'Product 1'

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
           </li>
         </ul>
       </nav>
-      <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-hidden">
+      <div className="w-[85%] p-10 pt-[5%] flex flex-wrap overflow-hidden">
         <div className="mr-3 mb-3 p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
           <div
             className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
@@ -36,147 +36,8 @@ function App() {
                 "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
             }}
           ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
-        </div>
-        <div className="p-3 border rounded shadow card w-[18%] h-[30vh] flex flex-col justify-center items-center">
-          <div
-            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-          ></div>
-          <h1>Lorem ipsum dolor sit.</h1>
+          <h1 className="hover:text-blue-300 ">Product 1</h1>
+          
         </div>
       </div>
     </div>
